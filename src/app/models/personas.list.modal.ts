602c8ed9ac41d4e71411cc1c2ba041d3aed5cf5d
Harold Velez <hvelez56@gmail.com>
2019-12-06 00:03:25 -0500

    cambios para usar el localstorage

--- src/app/models/personas.list.modal.ts
+++ src/app/models/personas.list.modal.ts
@@ -2,13 +2,18 @@ import { DeudasItem } from './deudas.item.modal';
 
 export class Personas{
 
+    id: number;
     nombre: string;
     deudas: DeudasItem[];
+    creadaEl: Date;
 
     constructor(nombre: string){
 
+        this.id = new Date().getTime();
         this.nombre = nombre;
-        this.deudas = [];   
+        this.deudas = [];
+        this.creadaEl = new Date();
+        
     }
 
 }
--- src/app/services/list-debts.service.ts
+++ src/app/services/list-debts.service.ts
@@ -8,15 +8,31 @@ export class ListDebtsService {
   personas: Personas[] = [];
 
   constructor() {
-    const lista1 = new Personas('Harold');
-    const lista2 = new Personas('Sebas');
+    // const lista1 = new Personas('Harold');
+    // const lista2 = new Personas('Sebas');
 
-    this.personas.push(lista1,lista2);
+    // this.personas.push(lista1,lista2);
+    this.getLocalStorage();
   }
 
   crearPersona(persona: string){
     const nuevaPersona = new Personas(persona);
 
     this.personas.push(nuevaPersona);
+
+    this.guardarEnStorage();
+
+  }
+
+  guardarEnStorage(){
+    localStorage.setItem('data', JSON.stringify(this.personas))
+  }
+
+  getLocalStorage(){
+    if(localStorage.getItem('data')){
+      this.personas = JSON.parse(localStorage.getItem('data'));
+    }else{
+      this.personas = [];
+    }
   }
 }
